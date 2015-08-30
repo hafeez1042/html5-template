613c98378110b1f0d3b416478c7574c16274a9bb
hafeez <hafeez@ichanical.com>
2015-08-30 13:16:05 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <title>Home Page</title>
+        <?php include_once "partials/_head.php" ?>
+    </head>
+    <body>
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include_once "partials/_header.php" ?>
+
+        <!-- Add your site or application content here -->
+        <div class="body">
+            <p>Body</p>
+        </div>
+
+        <?php include_once "partials/_footer.php" ?>
+    </body>
+</html>
